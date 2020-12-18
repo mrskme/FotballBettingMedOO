c3ab9ed9e2a6b359041f414a88df66e6f8e7c0a3
MrSkme <59914093+mrskme@users.noreply.github.com>
2020-12-18 10:33:55 +0100

    z<xc

---
+++ Tippekupong/Match.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tippekupong
+{
+    class Match
+    { 
+        private int _homeGoals;
+        private int _awayGoals;
+        private bool _matchIsRunning;
+        private string _bet;
+
+        public Match()
+        {
+            _matchIsRunning = true;
+        }
+
+        public void PlaceBet()
+        {
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+            _bet = Console.ReadLine();
+        }
+
+        public void PlayMatch()
+        {
+            while (_matchIsRunning)
+            {
+                Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+                var command = Console.ReadLine();
+                if (command == "X") StopMatch();
+                if (command == "H") AddGoal(true);
+                else if (command == "B") AddGoal(false);
+                Console.WriteLine(GetScore());
+            }
+        }
+        public void StopMatch()
+        {
+            _matchIsRunning = false;
+        }
+        public void AddGoal(bool isHomeTeam)
+        {
+            var addGoal = isHomeTeam ? _homeGoals++ : _awayGoals++;
+        }
+
+        public string GetScore()
+        {
+            return $"Stillingen er {_homeGoals}-{_awayGoals}.";
+        }
+
+        public string GetBetResult()
+        {
+            var result = _homeGoals == _awayGoals ? "U" : _homeGoals > _awayGoals ? "H" : "B";
+            var isBetCorrect = _bet.Contains(result);
+            var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+            return $"Du tippet {isBetCorrectText}";
+        }
+    }
+}
---
+++ Tippekupong/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tippekupong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Match match = new Match();
+            Match match2 = new Match();
+            match.PlaceBet();
+            match.PlayMatch();
+            Console.WriteLine(match.GetBetResult());
+
+            match2.PlaceBet();
+            match2.PlayMatch();
+            Console.WriteLine(match2.GetBetResult());
+        }
+    }
+}
